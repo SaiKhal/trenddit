95d72ec52ad7e5fa80084d88fb806b97af0c3dd3
Kaniz Ali <knzali3@gmail.com>
2018-02-01 15:13:23 -0500

    xcode project settings update

---
+++ trenddit/Controllers/PostViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PostViewController.swift
+//  trenddit
+//
+//  Created by C4Q on 2/1/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    
+    let postView = PostView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(postView)
+    }
+
+
+    // MARK: - Navigation
+
+
+}
